22fbb8687290ac4b6c1aad2dfc4ec28d9d6fdd3b
Paavan <paavanpatni99@gmail.com>
2025-09-09 13:44:45 +0530

    minor fixes

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
-      HEllo World
+    <div className="flex flex-col min-h-screen text-5xl text-white mx-50 mt-50 bg-background/95">
+      
     </div>
   );
 }
